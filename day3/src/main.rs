40b195918bb63dd6b349293d2592d583631632df
Thomas van Bruggen <Thomas.vanBruggen@HeidelbergEngineering.com>
2024-01-12 17:31:26 +0100

    wip

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-
+use std::collections::HashSet;
 
 fn main() {
     println!("Hello, world!");
@@ -8,29 +8,49 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filepath = &args[1];  
     let lines: Vec<String> = open(filepath);
-
-   //let lines = add_padding(lines); 
-    let width = lines[0].len();
-    let height = lines.len();
-    
+       
     let matrix = convert_to_matrix(lines);
 
     let mut symbol_coords = Vec::new();
+    let mut gears = Vec::new();
+
     for (row_ix, row) in matrix.iter().enumerate(){
         for (col_ix, c) in row.iter().enumerate(){
             if !(c.is_numeric() || *c == '.') {
-                println!("{} is a symbol on coord ({},{})", c, col_ix, row_ix);
-                symbol_coords.push((col_ix, row_ix));                
+                //println!("{} is a symbol on coord ({},{})", c, row_ix, col_ix);
+                symbol_coords.push((row_ix, col_ix));                
+            }
+            else if *c == '*'
+            {
+                gears.push((row_ix, col_ix));
             }
             else{
-                println!("{} is not a symbol", c);
+               // println!("{} is not a symbol", c);
             }
         }
     }
     // todo: iterate through symbols and look around the symbol for numbers
-    for (x,y) in symbol_coords{
-        iterate_surrounding_elements(&matrix, x,y);
+
+    let mut visited_coordinates: HashSet<(usize,usize)> = HashSet::new();
+    let mut numbers: Vec<i32> = Vec::new();
+   
+    for (row,col) in symbol_coords{
+        numbers.append(&mut iterate_surrounding_elements(&matrix, row, col, Some(&mut visited_coordinates)));
     }
+
+    let mut sum_of_ratios: i32 = 0;
+    for (row,col) in gears{
+        let surrounding: Vec<i32> = iterate_surrounding_elements(&matrix, row, col, None);
+        if surrounding.len() == 2
+        {
+            let ratio: i32 = surrounding[0]*surrounding[2];
+            sum_of_ratios += ratio;
+        }
+    }
+
+    let sum: i32 = numbers.iter().sum();
+    println!("The sum of all numbers bordering a symbol = {}", sum);
+    println!("Ths sum of gear rations = {}", sum_of_ratios);
 }
 
 fn convert_to_matrix(lines: Vec<String>) -> Vec<Vec<char>>
@@ -47,78 +67,79 @@ fn convert_to_matrix(lines: Vec<String>) -> Vec<Vec<char>>
     chars
 }
 
-fn iterate_surrounding_elements(matrix: &Vec<Vec<char>>, col: usize, row: usize) {
+fn iterate_surrounding_elements(matrix: &Vec<Vec<char>>, row: usize, 
+    col: usize, visited_coordinates: Option<&mut HashSet<(usize,usize)>>) -> Vec<i32>
+{
     let rows = matrix.len();
     let cols = matrix[0].len();
 
+    let mut numbers: Vec<i32> = Vec::new();
     // Iterate through surrounding elements
-    for i in (col.saturating_sub(1))..(col + 2).min(cols) {
-        for j in (row.saturating_sub(1))..(row + 2).min(rows) {
+    for i in (row.saturating_sub(1))..(row + 2).min(rows) {
+        for j in (col.saturating_sub(1))..(col + 2).min(cols) {
             // Skip the current element
-            if i == col && j == row {
+
+            let already_visited: bool = match visited_coordinates{
+                Some(vc) => vc.contains(&(i,j)),
+                None => false
+            };
+
+            if (i == row && j == col) || already_visited{
                 continue;
             }
 
             // Access the surrounding element at position (i, j)
-            let element = matrix[j][i];
-            let symbol = matrix[row][col];
+            let element = matrix[i][j];
+           
             // Perform operations with the surrounding element
             if element.is_numeric(){
-                println!("Element {} at ({}, {}) borders to the symbol {} at ({}, {})", element, i, j, symbol, row, col);              
+              //  println!("Element {} at ({}, {}) borders to the symbol {} at ({}, {})", element, i, j, symbol, row, col);              
 
                 // search left and right of element for other numerics
-                let nr = find_consecutive_numerics(&matrix[j], i);
-
-                println!("The whole number is {}", nr);
+                let nr = find_consecutive_numerics(&matrix, i, j, visited_coordinates);
+                numbers.push(nr);
+              //  println!("The whole number is {}", nr);
 
             }
             
         }
     }
+    numbers
 }
 
-fn find_consecutive_numerics(chars: &Vec<char>, x: usize) -> String {
+fn find_consecutive_numerics(matrix: &Vec<Vec<char>>, i: usize, j:usize, visited_coordinates: Option<&mut HashSet<(usize, usize)>>) -> i32 {
     let mut result = String::new();
+    let chars = &matrix[i];
 
     // Iterate to the left
-    let mut left = x;
+    let mut left = j;
     while left > 0 && chars[left - 1].is_numeric() {
         left -= 1;
     }
 
     // Iterate to the right
-    let mut right = x;
+    let mut right = j;
     while right < chars.len() - 1 && chars[right + 1].is_numeric() {
         right += 1;
     }
 
     // Construct the result string
-    for i in left..=right {
-        result.push(chars[i]);
+    for k in left..=right {
+
+        let _ = match visited_coordinates{
+            Some(vc) => vc.insert((i,k)),
+            None => false
+        };
+        
+        result.push(chars[k]);
     }
 
-    result
+    
+    let int_num = result.parse::<i32>().expect("error while converting &str to u32");
+
+    int_num
 }
 
-fn add_padding(lines: Vec<String>) -> Vec<String>
-{
-    let mut padded: Vec<String> = Vec::new();
-    let width = lines[0].len();
-    let height = lines.len();
-   
-    let binding = std::iter::repeat('.').take(width+2).collect::<String>();
-    let padding: &str = binding.as_str();
-    padded.push(String::from(padding));
-
-    for line in lines{
-        let padded_line = format!(".{}.", line);
-        padded.push(padded_line);
-    }
-
-    padded.push(String::from(padding));
-
-    padded
-}
 
 fn open(filepath: &str) -> Vec<String>
 {
@@ -130,19 +151,3 @@ fn open(filepath: &str) -> Vec<String>
 }
 
 
-
-
-#[test]
-fn it_works() {
-    let line: &str = "xxxxx";
-    let mut lines: Vec<String> = Vec::new();
-    lines.push(String::from(line));
-    lines.push(String::from(line));
-
-    let padded = add_padding(lines);
-
-    assert_eq!(padded[0], String::from("......."));
-    assert_eq!(padded[1], String::from(".xxxxx."));
-    assert_eq!(padded[1], padded[2]);
-    assert_eq!(padded[0], padded[3]);
-}
